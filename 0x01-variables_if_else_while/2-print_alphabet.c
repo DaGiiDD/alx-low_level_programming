fcf7d14bbfcad4165ab6b2ddfc4a7ad83ac98775
Dagim Derese <dagimderese11@gmail.com>
2022-09-11 10:32:29 -0700

    Variables in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - the main function
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - two loops that print the alphabet lower and upper case
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+	inr j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	         putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+	         putchar(j);
+        putchar('\n');
+	return (0);
+}
