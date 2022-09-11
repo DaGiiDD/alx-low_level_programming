190f99ff534330c8f3f2f07d25d93023b2070266
Dagim Derese <dagimderese11@gmail.com>
2022-09-11 08:17:06 -0700

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - this is the starting point of the programs
- * Return: returns 0
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like bulidinga multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	print("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
